3b7e32a05599c7b2ed6a6f183204979bdb46feee
24A31A05KI <karthikchaganti07@gmail.com>
2026-08-31 19:07:27 +0530

    Time: 2 ms (3.76%) | Memory: 19.2 MB (58.81%) - LeetSync

---
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
@@ -0,0 +1,6 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        l=[]
+        for i,j in zip(nums,index):
+            l.insert(j,i)
+        return l
