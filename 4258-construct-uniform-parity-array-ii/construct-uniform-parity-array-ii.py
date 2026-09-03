b7175db468a70c1923ac918da35c3d4043d844b5
24A31A05KI <karthikchaganti07@gmail.com>
2026-09-03 15:23:14 +0530

    Time: 11 ms (90.85%) | Memory: 35.4 MB (51.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,12 +1,9 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        mn = nums1[0]
-        can_odd = False
-        for i in nums1:
-            if i < mn:
-                mn = i
-            if i & 1:
-                can_odd = True
-        if mn & 1:
+        m=min(nums1)
+        if m&1:
             return True
-        return not can_odd
+        for n in nums1:
+            if n&1:
+                return False
+        return True
