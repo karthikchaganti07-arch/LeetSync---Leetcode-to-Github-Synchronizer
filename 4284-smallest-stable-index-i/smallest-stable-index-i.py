6d3037cb1ae09712300789470fbe60386248aaee
24A31A05KI <karthikchaganti07@gmail.com>
2026-09-04 16:14:59 +0530

    Time: 5 ms (51.72%) | Memory: 19.4 MB (9.85%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.py
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -1,10 +1,9 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         n=len(nums)
-        res=[]
-        for i in range(len(nums)):
+        for i in range(n):
             l=max(nums[:i+1])
-            r=min(nums[i:n])
+            r=min(nums[i:])
             if l-r<=k:
                 return i
         return -1
