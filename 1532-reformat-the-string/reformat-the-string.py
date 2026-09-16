eff513a8de45a8b07c12f2a0ced8c797c38fa8ec
24A31A05KI <karthikchaganti07@gmail.com>
2026-09-16 20:13:57 +0530

    Time: 4 ms (64.85%) | Memory: 19.4 MB (25.76%) - LeetSync

---
+++ 1532-reformat-the-string/reformat-the-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def reformat(self, s: str) -> str:
+        if len(s)<2:
+            return s
+        num=[]
+        alpha=[]
+        for i in s:
+            if i.isdigit():
+                num.append(i)
+            else:
+                alpha.append(i)
+        if not num or not alpha:
+            return ""
+        n=len(num)
+        a=len(alpha)
+        if n-a>2 or a-n>2:
+            return ""
+        if abs(n - a) > 1:
+            return ""
+        res = []
+        if n >= a:
+            for i in range(a):
+                res.append(num[i])
+                res.append(alpha[i])
+            if n > a:
+                res.append(num[-1])
+        else:
+            for i in range(n):
+                res.append(alpha[i])
+                res.append(num[i])
+            res.append(alpha[-1])
+        return "".join(res)
