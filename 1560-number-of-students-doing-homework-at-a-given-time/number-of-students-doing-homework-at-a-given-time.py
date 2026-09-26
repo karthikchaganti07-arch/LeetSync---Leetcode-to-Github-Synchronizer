556acb26baaeb4fdb7c4026eeddc7d5870a38dd9
24A31A05KI <karthikchaganti07@gmail.com>
2026-09-26 15:32:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (26.60%) - LeetSync

--- 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.py
+++ 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.py
@@ -1,3 +1,7 @@
 class Solution:
     def busyStudent(self, startTime: list[int], endTime: list[int], queryTime: int) -> int:
-        return sum(s <= queryTime <= e for s, e in zip(startTime, endTime))
+        count=0
+        for i in range(len(startTime)):
+            if startTime[i]<=queryTime<=endTime[i]:
+                count+=1
+        return count
